05df9a1078cfd18ca4b8c4bf81f05b76c0262876
Alan Swircenski <ahswircenski@gmail.com>
2016-05-02 00:09:32 -0800

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,22 @@
+//**************************************************************
+// Given the length and width of a rectangle, this C++ program
+// computes and outputs the perimeter and area of the rectangle.
+//**************************************************************
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int myFavNumbers[5];
+
+	int someNumbers[5] = { 3, 5, 7, 12, 46 };
+
+	cout << someNumbers[4];
+	
+	cout << endl;
+	cout << "END" << endl;
+	//cin.get(); // force a pause in the console window.
+	system("Pause");
+	return 0;
+}
